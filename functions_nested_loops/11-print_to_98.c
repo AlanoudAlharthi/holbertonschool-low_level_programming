8e9819e6497db8cccc3b9a37d0e829ac37516edf
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-08-30 18:55:28 +0000

    Add function print_to_98 to print numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,48 +1,35 @@
+#include <stdio.h>
 #include "main.h"
-
-/**
- * print_number - prints an integer using _putchar
- * @n: number to print
- */
-void print_number(int n)
-{
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
-}
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
+ *
+ * Description: Numbers are separated by a comma and a space.
+ * The sequence is in order, ending with 98 and a new line.
  */
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i < 98; i++)
-        {
-            print_number(i);
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-    else
-    {
-        for (i = n; i > 98; i--)
-        {
-            print_number(i);
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-
-    print_number(98);
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
+		}
+	}
 }
