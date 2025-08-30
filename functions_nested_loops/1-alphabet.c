55c4d2fc7d7f6a2bee963e9d9763b91e284ffba1
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-08-30 18:49:35 +0000

    Add 1-alphabet.c function and update main.h, _putchar.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the lowercase alphabet followed by a new line
+ * print_alphabet - prints the alphabet in lowercase,
+ * followed by a new line
  *
  * Return: void
  */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        _putchar(c);
-    _putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
