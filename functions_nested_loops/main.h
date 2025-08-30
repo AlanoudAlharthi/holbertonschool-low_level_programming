4c52f6a5ef1750b9dbd2db0e3d508018ea3d1a89
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-08-30 11:26:51 +0000

    task11

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,7 +6,4 @@
 /* _putchar: writes the character c to stdout */
 int _putchar(char c);
 
-/* print_to_98: prints all natural numbers from n to 98 */
-void print_to_98(int n);
-
 #endif /* MAIN_H */
