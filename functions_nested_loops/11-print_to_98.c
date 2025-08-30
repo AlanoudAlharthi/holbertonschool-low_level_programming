b28c9f046fe7870f1fef63b7a393ee18110b7904
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-08-30 19:01:54 +0000

    Fix 11-print_to_98: use _putchar only, Betty-style docstrings

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,31 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * print_number - print a long integer using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
+static void print_number(long n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((char)(n % 10 + '0'));
+}
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
  *
- * Description: Numbers are separated by a comma and a space.
- * The sequence ends with 98 followed by a new line.
+ * Description: Numbers are separated by a comma and a space,
+ * and the sequence ends with 98 followed by a new line.
+ *
+ * Return: void
  */
 void print_to_98(int n)
 {
@@ -14,22 +33,24 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
+			print_number((long)i);
+			_putchar(',');
+			_putchar(' ');
 		}
+		print_number(98L);
+		_putchar('\n');
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else
-				printf("%d\n", i);
+			print_number((long)i);
+			_putchar(',');
+			_putchar(' ');
 		}
+		print_number(98L);
+		_putchar('\n');
 	}
 }
