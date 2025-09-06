85367fdbbd91c7084a96c11d87126f678d13027e
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-06 19:37:04 +0000

    task2

--- pointers_arrays_strings/_strncpy.c
+++ pointers_arrays_strings/_strncpy.c
@@ -4,25 +4,19 @@
  * _strncpy - copies a string
  * @dest: destination buffer
  * @src: source string
- * @n: maximum number of bytes to copy
+ * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
+    int i;
 
-    while (i < n && src[i] != '\0')
-    {
+    for (i = 0; i < n && src[i] != '\0'; i++)
         dest[i] = src[i];
-        i++;
-    }
 
-    while (i < n)
-    {
+    for (; i < n; i++)
         dest[i] = '\0';
-        i++;
-    }
 
     return (dest);
 }
