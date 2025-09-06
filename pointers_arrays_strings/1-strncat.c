938df5544d6da015b6666d17f214ff2005e9bfcb
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-06 19:30:23 +0000

    task2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,36 +1,32 @@
-#include "main.h"
-
 /**
  * _strncat - concatenates two strings using at most n bytes from src
- * @dest: pointer to the destination string
- * @src: pointer to the source string
- * @n: maximum number of bytes to use from src
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of bytes to copy
  *
  * Return: pointer to dest
- *
- * Description: Appends the string pointed to by src to the end
- *              of the string pointed to by dest, using at most n
- *              bytes from src. Overwrites the terminating null
- *              byte at the end of dest and adds a terminating
- *              null byte.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-	/* العثور على نهاية dest */
+	/* move to the end of dest */
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
 
-	/* نسخ محتوى src حتى n حرف */
-	while (src[j] != '\0' && j < n)
+	/* copy at most n characters from src */
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	dest[i] = '\0'; /* إضافة null byte في النهاية */
+	/* add null terminator */
+	dest[i] = '\0';
 
 	return (dest);
 }
--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string
- * @s: Pointer to the string
+ * _strlen - returns the length of a string
+ * @s: the string
  *
- * Return: Length of string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
