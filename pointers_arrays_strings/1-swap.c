bba07faa0e8ec675f121c759edef0b4dbd5dc4b7
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-06 17:38:19 +0000

    task1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,12 +4,15 @@
  * swap_int - swaps the values of two integers
  * @a: pointer to first integer
  * @b: pointer to second integer
+ *
+ * Description: This function swaps the values of the integers
+ *              pointed to by a and b using a temporary variable.
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
+	int temp;
 
-    temp = *a;  /* حفظ قيمة a في متغير مؤقت */
-    *a = *b;    /* نقل قيمة b إلى a */
-    *b = temp;  /* نقل القيمة المحفوظة في temp إلى b */
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
