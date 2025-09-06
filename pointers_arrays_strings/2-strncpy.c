6a737eeca0b67b0e45050092378b341968e246f9
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-06 19:41:14 +0000

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: destination buffer
- * @src: source string
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
- */
+* _strncpy - copies a string
+* @dest: destination buffer
+* @src: source string
+* @n: number of bytes to copy
+*
+* Return: pointer to dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
     int i;
