3fc207d1a1e850f03ca434f32cfd476001c03616
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-11 14:40:35 +0000

    task0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
 
-#endif /* MAIN_H */
+#endif
