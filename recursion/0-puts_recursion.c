43073f5d63fcab0c650f2df5e14a6bc6c9b6298d
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-13 14:53:34 +0000

    task0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,15 +3,16 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: the string to print
+ *
+ * Return: nothing
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s == '\0')   /* base case: end of string */
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);        /* print current character */
+	_puts_recursion(s + 1); /* recursive call with next char */
 }
