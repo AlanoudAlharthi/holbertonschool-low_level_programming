024c41c7fa6eea10f7cfea813aff8b12aa5e2715
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-25 10:22:34 +0000

    task0

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-/* print_name_as_is and print_name_uppercase are defined here for testing */
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
 void print_name_as_is(char *name)
 {
     printf("Hello, my name is %s\n", name);
 }
 
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
 void print_name_uppercase(char *name)
 {
-    unsigned int i = 0;
+    unsigned int i;
 
     printf("Hello, my uppercase name is ");
+    i = 0;
     while (name[i])
     {
         if (name[i] >= 'a' && name[i] <= 'z')
@@ -23,9 +35,15 @@ void print_name_uppercase(char *name)
     putchar('\n');
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     print_name("Bob", print_name_as_is);
     print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
     return (0);
 }
--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a given function
- * @name: name to print
- * @f: pointer to function that takes a char* and returns void
+ * print_name - prints a name using a given function pointer
+ * @name: name of the person
+ * @f: function pointer that takes a char * and returns void
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
-
-    f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
