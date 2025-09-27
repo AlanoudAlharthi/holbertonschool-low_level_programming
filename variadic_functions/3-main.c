50b23b6c7f48fddc1283956441b0a7c10bc29140
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-27 15:31:05 +0000

    task3

--- variadic_functions/3-main.c
+++ variadic_functions/3-main.c
@@ -1,17 +1,12 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
- * main - test the print_all function
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    print_all("fcsi", 3.14435, 'H', "#Cisfun", 0);
-    print_all("mnbvfqcepolsbxzi", 3.14435, 'H', "#Cisfun", 0);
-    print_all("qwertyuopadghjklzxvbnm");
-    print_all("");
-    print_all(NULL);
-    print_all("fcsi", 3.14435, 'H', NULL, 402);
+    print_all("ceis", 'B', 3, "stSchool");
     return (0);
 }
