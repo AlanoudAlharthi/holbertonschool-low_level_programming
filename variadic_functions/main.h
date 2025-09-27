560ad7130312634eb40900b322503b56020279e8
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-27 15:21:57 +0000

    task3

---
+++ variadic_functions/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/* Prototype for the print_all function */
+void print_all(const char * const format, ...);
+
+#endif /* MAIN_H */
