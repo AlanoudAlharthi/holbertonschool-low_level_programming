457fc17eb1e0e035ce870b3e0640ec889cf76080
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-27 15:11:06 +0000

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,7 +5,6 @@
 /**
  * print_all - prints anything based on format string
  * @format: list of types of arguments passed
- *   c: char, i: integer, f: float, s: char *
  *
  * Return: Nothing
  */
@@ -13,29 +12,34 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
 	char *sep = "";
+	char *str;
 
 	va_start(args, format);
 
 	if (format != NULL)
 	{
-		while (format[i] != '\0')
+		while (format[i])
 		{
-			if (format[i] == 'c')
-				printf("%s%c", sep, va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%s%d", sep, va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%s%f", sep, va_arg(args, double));
-			if (format[i] == 's')
+			if (format[i] == 'c' || format[i] == 'i' ||
+			    format[i] == 'f' || format[i] == 's')
 			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", sep, str);
+				printf("%s", sep);
+				if (format[i] == 'c')
+					printf("%c", va_arg(args, int));
+				if (format[i] == 'i')
+					printf("%d", va_arg(args, int));
+				if (format[i] == 'f')
+					printf("%f", va_arg(args, double));
+				if (format[i] == 's')
+				{
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s", str);
+				}
+				sep = ", ";
 			}
-			sep = ", ";
 			i++;
 		}
 	}
