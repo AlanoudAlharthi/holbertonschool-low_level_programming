6cc4c7081daaf0adbba6368d0da796cc87ba1fca
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-27 15:24:13 +0000

    task3

--- variadic_functions/3-main.c
+++ variadic_functions/3-main.c
@@ -1,7 +1,14 @@
-#include "variadic_functions.h"
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    print_all("ceis", 'B', 3, "stSchool");
+    print_all("ceis", 'B', 3, 3.14, "stSchool");
+    print_all("sifc", NULL, 42, 2.718, 'H');
+    print_all("abcxyz", 'X', 10, "Hello"); /* unknown specifiers are ignored */
     return (0);
 }
