03516c29542a909ddc27c811632d41456db1ed9f
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-08 10:25:27 +0000

    task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,13 +1,13 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the head of the list
  * @str: string to duplicate and store in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -17,16 +17,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (str == NULL)
 		return (NULL);
 
-	/* حساب طول النص */
-	while (str[len])
+	while (str[len] != '\0')
 		len++;
 
-	/* حجز مساحة للـ node الجديدة */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* نسخ النص باستخدام strdup */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -35,8 +32,8 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = len;
-	new_node->next = *head; /* تربط node الجديدة بالرأس القديم */
-	*head = new_node;       /* تحديث الرأس ليشير للعقدة الجديدة */
+	new_node->next = *head;
+	*head = new_node;
 
 	return (new_node);
 }
