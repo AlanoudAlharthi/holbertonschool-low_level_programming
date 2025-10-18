0c80386e3f6f63dd40e7684327e10ef7965d8703
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-18 16:16:34 +0000

    task3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,12 +18,6 @@ void error_exit(int code, const char *msg, const char *arg)
     exit(code);
 }
 
-/**
- * main - Copies the content of a file to another file
- * @argc: Number of arguments
- * @argv: Array of argument strings
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
 {
     int fd_from, fd_to;
@@ -36,12 +30,10 @@ int main(int argc, char *argv[])
         exit(97);
     }
 
-    /* Open source file */
     fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1)
         error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-    /* Open/create destination file with rw-rw-r-- permissions */
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
     {
@@ -50,7 +42,6 @@ int main(int argc, char *argv[])
         error_exit(99, "Error: Can't write to %s\n", argv[2]);
     }
 
-    /* Read from source and write to destination safely */
     while (1)
     {
         n_read = read(fd_from, buffer, sizeof(buffer));
@@ -62,9 +53,8 @@ int main(int argc, char *argv[])
                 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
             error_exit(98, "Error: Can't read from file %s\n", argv[1]);
         }
-
-        if (n_read == 0) /* EOF */
-            break;
+        if (n_read == 0)
+            break; /* EOF */
 
         n_written = write(fd_to, buffer, n_read);
         if (n_written != n_read)
@@ -77,7 +67,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    /* Close file descriptors */
     if (close(fd_from) == -1)
         error_exit(100, "Error: Can't close fd %d\n", argv[1]);
     if (close(fd_to) == -1)
