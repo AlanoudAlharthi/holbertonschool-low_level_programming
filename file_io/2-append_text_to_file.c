2339475912166f61229fd569a84d32d3f4ec6d0d
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-18 15:48:48 +0000

    task2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,39 +1,40 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
- * @text_content: NULL-terminated string to add at the end
+ * @text_content: text to add
  *
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t len, written;
+	int fd, w;
+	size_t len;
 
-    if (!filename)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content)
-    {
-        len = strlen(text_content);
-        written = write(fd, text_content, len);
-        if (written != len)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		w = write(fd, text_content, len);
+		if (w != (int)len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
