eb79e16b182e99445f0955bdd4e0b89b4aa56b3d
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-18 15:43:07 +0000

    task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file and writes text_content into it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0, written;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		/* Calculate length of text_content manually */
+		while (text_content[len])
+			len++;
+	}
+
+	/* Open file with rw------- permissions, truncate if it exists */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (len > 0)
+	{
+		written = write(fd, text_content, len);
+		if (written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <unistd.h>
+#include <sys/types.h>
 
+/* Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
