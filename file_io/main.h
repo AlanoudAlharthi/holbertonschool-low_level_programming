bbca3e46e4c2ecdcf77a9bb533b7fb8a8b804f4e
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-18 15:59:18 +0000

    task3

--- file_io/main.h
+++ file_io/main.h
@@ -2,11 +2,18 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <sys/types.h>
+#include <unistd.h>
 
-/* Prototypes */
+/* 0. Tread lightly, she is near */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1. Under the snow */
 int create_file(const char *filename, char *text_content);
+
+/* 2. Speak gently, she can hear */
 int append_text_to_file(const char *filename, char *text_content);
+
+/* 3. cp */
+int main(int ac, char **av);
 
 #endif /* MAIN_H */
