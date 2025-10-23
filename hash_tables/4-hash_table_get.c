5e1fd4d4f56588ef7970d7f6fc8bd1b3ce937242
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-23 15:45:48 +0000

    task4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -5,14 +5,14 @@
  * @ht: the hash table
  * @key: the key to look for
  *
- * Return: the value associated with the key, or NULL if not found
+ * Return: value associated with the key, or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,15 +1,15 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * @key: The key (string)
+ * @value: The value associated with the key
+ * @next: Pointer to the next node in the list
  */
 typedef struct hash_node_s
 {
@@ -20,10 +20,8 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use chaining collision handling
+ * @size: Size of the array
+ * @array: Array of pointers to hash nodes
  */
 typedef struct hash_table_s
 {
@@ -31,10 +29,10 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-/* Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /* HASH_TABLES_H */
